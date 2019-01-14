1e02983d992965e0f3820c13a4ee1b5312daff3a
zhanshun <shunde.zhangqweb.de>
2019-01-14 10:34:45 +0100

    highsciree

---
+++ src/model/Score.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Score {
+    private String name;
+    private String wins;
+
+    public Score( String name, String wins){
+        this.name = name;
+        this.wins = wins;
+    }
+
+    public String getName(){
+
+    }
+
+    public String getWins(){
+        return wins;
+    }
+
+    public void setWins(String wins){
+        this.wins = wins;
+    }
+}
