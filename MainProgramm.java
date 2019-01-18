abee5c04bfd72f85dfa0d47cecfdb59a94950818
zhanshun <shunde.zhangqweb.de>
2019-01-18 09:10:41 +0100

    highsciree

--- MainProgramm.java
+++ MainProgramm.java
@@ -1,7 +1,7 @@
 import akkgframework.control.fundamental.MainController;
 import view.MainView;
 import view.StartpanelScreen;
-
+//Efi
 public class MainProgramm {
     public static void main(String[] args) {
         MainView view = new MainView(new MainController());
