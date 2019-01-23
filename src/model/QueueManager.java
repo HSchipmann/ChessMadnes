52aadf024bdd9b9911e04afbbe14a0f5fbe818a3
HSchipmann <henribooom@gmail.com>
2019-01-23 13:14:29 +0100

    Letzter Push der Stunde.

--- src/model/QueueManager.java
+++ src/model/QueueManager.java
@@ -7,6 +7,7 @@ public class QueueManager {
     private PowerUp powerUp;
     private Queue<PowerUp> powerUpQueue;
 
+    //fügt ein neues random PowerUp in die Queue
     public String addNewPowerUp(){
         PowerUp powerUp = new PowerUp();
         powerUpQueue.enqueue(powerUp);
@@ -14,6 +15,8 @@ public class QueueManager {
         return powerUp.getImage();
     }
 
+
+    //Entfernt das erste PowerUp aus der Queue und returnt es danach
     public PowerUp realesePowerUp(){
         PowerUp topPowerup= powerUpQueue.front();
         if(!powerUpQueue.isEmpty()){
--- src/model/Schachbrett.java
+++ src/model/Schachbrett.java
@@ -8,18 +8,13 @@ import akkgframework.view.DrawTool;
 public class Schachbrett extends GraphicalObject {
 
     private Tileset tileset;
-    private int[][] mapArray;
+    public int[][] mapArray;
 
 
     public Schachbrett(){
         //tileset benutzen um Bild einfach zu zeichnen
         tileset = new Tileset("src/assets/whiteTile.png",150,150);
         createMapArray();
-    }
-
-    //Bild zeichnen
-    public void drawTool(DrawTool drawTool){
-        drawTool.drawImage(tileset.getTile(0,1),0,0);
     }
 
     //Map erstellen in der kein Platz belegt ist
@@ -32,10 +27,13 @@ public class Schachbrett extends GraphicalObject {
     }
 
 
+
     public int[][] changeMapArray(){
 
         return null;
     }
+
+
 
 
 
